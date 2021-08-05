82df8c0f32ceb810273719cc8cee6d82b9f5a9dc
lando-stan <mohamedguled@yahoo.com>
2021-08-05 14:21:28 -0500

    prompt

--- script.js
+++ script.js
@@ -1,21 +1,101 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
+let lowerCase = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+let upperCase = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+let characters = [
+  '?',
+  '@',
+  '%',
+  '+',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+
+function passwordPrompt(){
+var passwordLength = parseInt(prompt("Please choose a password between 8-128 characters."))
+}
+
+var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
 
-
-
-
-
-
-
-
-
-
   
-return "test";
-
+  
 }
 // Write password to the #password input
 function writePassword() {
