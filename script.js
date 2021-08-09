8f66c16f53bb5ebc149f1020dd1ca0a9bed71b9b
lando-stan <mohamedguled@yahoo.com>
2021-08-09 13:27:51 -0500

    2nd commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 
+
+let generatedArray = []
 let lowerCase = [
   'a',
   'b',
@@ -84,19 +86,65 @@ let characters = [
 ];
 
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
+let passwordLength;
 
 function passwordPrompt(){
-var passwordLength = parseInt(prompt("Please choose a password between 8-128 characters."))
+passwordLength = parseInt(prompt("Please choose a password between 8-128 characters."))
+// always try to console log so you're not flying blind
+console.log(passwordLength);
+
+  // i need to have the user let me know which character types they want.
+  // we can do this through something called a confirm prompt
+  let upperCaseConfirm = confirm("Do you want Upper Case characters?");
+  console.log(upperCaseConfirm);
+
+  let lowerCaseConfirm =  confirm("Do you want Lower Case characters?");
+  let specialConfirm = confirm("Do you want Special characters?");
+  let numbersConfirm = confirm("Do you want Number characters?");
+
+  // i can set up a conditional because I now have access to what the user chose
+
+  
+
+  if (upperCaseConfirm === true){
+    // I need to grab the characters from the uppercase array and store them 
+    // I will put them in a new array 
+    // you can concat the uppercase array to an empty array
+     generatedArray = generatedArray.concat(upperCase);
+    
+
+  };
+  
+
+
+if (lowerCaseConfirm === true){
+  generatedArray = generatedArray.concat(lowerCase);
+  
+}
+
+if (specialConfirm === true){
+  generatedArray = generatedArray.concat(characters);
+  
+}
+
+if (numbersConfirm === true){
+  generatedArray = generatedArray.concat(numbers);
+}
+console.log(generatedArray)
 }
 
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
+  passwordPrompt(); // this is now working and I am getting the password length
 
-  
-  
+
+  finalPassword = "";// whatever our random generated string is
+ 
+   
+  return finalPassword
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
